50a9d5e8c89f915703babe9f205b5c65c1cf21c8
Bobby Tanwer <Bobby.Tanwer@BHEngagement.com>
2017-10-02 13:32:19 -0500

    adding changes 10/2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'CookBook';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { RecipeComponent } from './components/recipe/recipe.component';
+import { CookComponent } from './components/cook/cook.component';
+import { NotfoundcomponentComponent } from './components/notfoundcomponent/notfoundcomponent.component';
+import { UploadrecipeComponent } from './components/uploadrecipe/uploadrecipe.component';
+import { LoginComponent } from './components/login/login.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    RecipeComponent,
+    CookComponent,
+    NotfoundcomponentComponent,
+    UploadrecipeComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+
+    RouterModule.forRoot([
+      {path: '', component: HomeComponent},
+      {path: 'uploadrecipe', component: UploadrecipeComponent},
+      {path: 'recipe', component: RecipeComponent},
+      {path: 'cook', component: CookComponent},
+      {path: 'login', component: LoginComponent},
+      {path: '**', component: NotfoundcomponentComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
