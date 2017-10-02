f90f0ec70db7a38ee9354c4ad1e7d6cf289ac441
Bobby Tanwer <Bobby.Tanwer@BHEngagement.com>
2017-10-02 13:34:51 -0500

    added components and routes for the same

---
+++ src/app/components/uploadrecipe/uploadrecipe.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-uploadrecipe',
+  templateUrl: './uploadrecipe.component.html',
+  styleUrls: ['./uploadrecipe.component.css']
+})
+export class UploadrecipeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
